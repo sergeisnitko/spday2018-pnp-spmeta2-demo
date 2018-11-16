b9037579ec106ace059504baf7b7ba80ed54b62e
Sergei Snitko <s.snitko@gmail.com>
2018-11-16 23:02:00 +0300

    added spmeta2 demo model

---
+++ SPMeta2/ey.m2.core/Deploy.cs
@@ -0,0 +1,76 @@
+﻿using spmeta2.demo.Artefacts;
+using spmeta2.demo.Common;
+using SPF.M2;
+using Microsoft.SharePoint.Client;
+using SPMeta2.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spmeta2.demo
+{
+    public static class Deploy
+    {
+        /**
+         * 
+         * ======================
+         * Deploy scenarious demo
+         * ======================
+         * 
+         */
+
+
+        /**
+        * 
+        * Base provisioning example
+        * Usually a provisioning project contains one provisioning scenario
+        * 
+        */
+        public static void Demo(ClientContext Ctx)
+        {
+            var SiteDemoModel = ModelInfo.SiteDemoModel();
+            var WebDemoModel = ModelInfo.WebDemoModel();
+
+            SiteDemoModel.SetIncrementalProvisionModelId("Demos Site Demo");
+            WebDemoModel.SetIncrementalProvisionModelId("Demos Web Demo");
+
+            Ctx.DeployModel(SiteDemoModel);
+            Ctx.DeployModel(WebDemoModel);
+        }
+
+        /**
+        * 
+        * Import data example as a stand along process
+        * 
+        */
+        public static void ImportDemo(ClientContext Ctx)
+        {
+            var WebDemoDataModel = ModelInfo.WebDemoDataModel();
+            WebDemoDataModel.SetIncrementalProvisionModelId("Demos Web Demo Data");
+            Ctx.DeployModel(WebDemoDataModel);
+        }
+
+        /**
+        * 
+        * Example for force retraction model
+        * The example removes custom action
+        * 
+        */
+        public static void DemoRetractLite(ClientContext Ctx)
+        {
+            var Site = Ctx.Site;
+
+            var CustomActions = Site.UserCustomActions;
+            Ctx.Load(CustomActions);
+            Ctx.ExecuteQuery();
+            var SettingsLinkAction = CustomActions.Where(x => x.Name == ActionsInfo.M2ArtefactsAction().Name).FirstOrDefault();
+            if (SettingsLinkAction != null)
+            {
+                SettingsLinkAction.DeleteObject();
+                Ctx.ExecuteQuery();
+            }
+        }
+    }
+}
---
+++ SPMeta2/ey.m2.core/DynamicAssets/Demos/M2.Demo/Demos.m2.artefacts.js
@@ -0,0 +1,3 @@
+﻿/**
+ * This JS file is populated with the dynamic content from the model definition
+ */
